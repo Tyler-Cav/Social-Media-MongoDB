2d63d7d2b8ec381c380f27deb620f8c6e57c7c76
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-24 20:59:58 -0500

    added all model files, Index.js and User.js are complete

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      UserName: {
+        type: String,
+        unique: true, 
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: Boolean,
+        default: true,
+        unique: true, 
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+      },
+      thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }],
+      friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      }],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
+  userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  })
+  
+  const User = model('user', userSchema);
+  
+  module.exports = User;
