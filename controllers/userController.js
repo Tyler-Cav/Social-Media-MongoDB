0fe0cb64995c1ac3581b945bf9e6f959a45dba19
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-01-26 00:17:15 -0500

    updated more routes for users. The friend adding and removing works. But cant get it to populate more details

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,18 @@ module.exports = {
             res.status(500).json(err);
         }
     },
+    //Get a single user
+    async oneUser(req, res) {
+        try {
+            const getOne = await User.findOne({ _id: req.params.userId })
+                .select('-__v')
+                .populate('thoughts', 'friends')
+            res.status(200).json(getOne)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    },
     //Create a new user
     async createUser(req, res) {
         try {
@@ -23,15 +35,51 @@ module.exports = {
             return res.status(500).json(err);
         }
     },
-    //Get a single user
-    async oneUser(req, res) {
+    //Add a friend for a user
+    async addFriend(req, res) {
         try {
-            const getOne = await User.findOne({ _id: req.params.userId })
-                .select('-__v')
-            res.status(200).json(getOne)
+            const addFriend = await User.updateOne(
+                {_id: req.params.userId }, 
+                {$addToSet: {friends: req.params.friendId} }
+            )
+            
+             res.json(addFriend)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    //remove a friend
+    async removeFriend(req, res) {
+        try {
+            const removeFriend = await User.updateOne(
+                {_id: req.params.userId }, 
+                {$pull: {friends: req.params.friendId} }
+            )
+             res.json(removeFriend)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+    //Update a user
+    async updateUser(req, res) {
+        try {
+            const updateUser = await User.updateOne(req.body);
+            res.json(updateUser);
         } catch (err) {
             console.log(err);
             return res.status(500).json(err)
         }
     },
+    //Delete User
+    async deleteUser(req, res) {
+        try {
+            const deleteUser = await User.deleteOne({ _id: req.params.userId })
+            res.status(200).json(deleteUser)
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err)
+        }
+    }
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -3,11 +3,21 @@ const {
     getUser,
     createUser,
     oneUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
  } = require('../../controllers/userController')
 
 router.route('/').get(getUser).post(createUser)
 
 router.route('/:userId')
- .get(oneUser)
+.get(oneUser)
+.put(updateUser)
+.delete(deleteUser)
+
+router.route('/:userId/friends/:friendId')
+.put(addFriend)
+.delete(removeFriend)
 
 module.exports = router;
